1c36453a859903b080cbcc519bcaceb5cc8a2fc0
Gabriel Amorim <90463088+GabrielAmorim08@users.noreply.github.com>
2025-01-30 21:47:33 -0300

    subindo como pasta

---
+++ Projeto_Kalibre/VO/CalculoVO.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto_Kalibre.VO
+{
+    public class CalculoVO
+    {
+        public double? Peso { get; set; }
+        public double? PesoBisnagas { get; set; }
+        public double? Densidade { get; set; }
+        public double? Resultado { get; set; }
+        public string? TipoPeso { get; set; }
+    }
+}
